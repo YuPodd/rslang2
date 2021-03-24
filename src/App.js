fc787b83efb21e9567b363e0eeafeebcfac545b1
Yuliya <yulia.podd@gmail.com>
2021-03-24 15:43:19 +0200

    feat: add routing to units

--- src/App.js
+++ src/App.js
@@ -9,11 +9,17 @@ import StudyPage from './components/studyPage/StudyPage';
 import GamesPage from './components/gamesPage/GamesPage';
 import StatisticsPage from './components/statisticsPage/statisticsPage';
 import Header from './components/Header';
-import Footer from './components/Footer'
-
+import Footer from './components/Footer';
+import Unit from './components/studyPage/Unit'
+import {numberOfUnits} from "./constants/constants"
 
 function App() {
-  return (
+ const unitRoute = numberOfUnits.map((number) => {
+    return (
+      <Route path={`/unit${number}`} component={Unit}/>
+    )
+  });
+   return (
     <div className="App">
       <Header/>
       <Switch>
@@ -21,6 +27,7 @@ function App() {
         <Route path='/studyPage' component={StudyPage} />
         <Route path='/gamesPage' component={GamesPage} />
         <Route path='/statisticsPage' component={StatisticsPage} />
+        {unitRoute}
         <Redirect from='/' to='/mainPage'/>
       </Switch>
         <Footer/>
--- src/components/studyPage/StudyPage.js
+++ src/components/studyPage/StudyPage.js
@@ -1,20 +1,25 @@
 import "animate.css/animate.css";
+import { Link } from "react-router-dom";
+import {numberOfUnits} from "../../constants/constants"
 
 export default function StudyPage() {
-  const numberOfUnits = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-  ];
+   
   const unit = numberOfUnits.map((number) => {
-    return <div className="unit_div animate__animated animate__backInLeft" id="number"><a href="#">Unit {number}</a></div>;
+    return (
+      <div
+        className="unit_div animate__animated animate__backInLeft"
+        id={number}
+        key={number}
+      >
+        <Link to={`/unit${number}`}>Unit {number}</Link>
+      </div>
+    );
   });
-  return <div className="study_wrapper">
-      <div className="units_wrapper">
-      {unit}
-      
-      </div></div>;
+
+  return (
+    
+    <div className="study_wrapper">
+      <div className="units_wrapper">{unit}</div>
+    </div>
+  );
 }

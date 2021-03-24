5305b2930e798c856db49a904b531f7759478968
Yuliya <yulia.podd@gmail.com>
2021-03-24 22:11:05 +0200

    feat: add fetch from api

--- src/components/studyPage/Unit.js
+++ src/components/studyPage/Unit.js
@@ -1,9 +1,45 @@
 import { useParams } from "react-router";
+import {useEffect, useState} from 'react';
+import { Fragment } from "react";
 
-export default function Unit(){
-    let {number} = useParams();
+export default function Unit() {
+  let { number } = useParams();
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://react-learnwords-example.herokuapp.com/words?group=${number-1}&page=1`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
     return (
-    <div>Unit {number}</div>
+        <Fragment>
+            <div>Unit {number}</div>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.word} {item.wordTranslate}
+          </li>
+        ))}
+      </ul>
+      </Fragment>
     );
- 
+  }
 }

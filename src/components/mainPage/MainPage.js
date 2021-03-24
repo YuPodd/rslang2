952f309664dd9d9e9b701e095de427c6fd558d8d
Yuliya <yulia.podd@gmail.com>
2021-03-24 13:56:46 +0200

    fix: fix responsibility

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -1,11 +1,12 @@
 import Video from "./Video";
+import "animate.css/animate.css";
 
 export default function MainPage() {
   return (
     <div className="main_page_wrapper">
       <section className="main_page_about">
        
-        <div className="about_text">
+        <div className="about_text animate__animated animate__backInDown">
         <h1>Welcome to RS Lang!</h1>
         <p>
           RS Lang - это уникальное приложение для изучения английского.

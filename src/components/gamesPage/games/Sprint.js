e641a2bc77cf4c5f86a47a035d9fdb682d319eb4
Yuliya <yulia.podd@gmail.com>
2021-03-29 15:29:17 +0300

    feat: add start html to the game

--- src/components/gamesPage/games/Sprint.js
+++ src/components/gamesPage/games/Sprint.js
@@ -1,5 +1,30 @@
+import { Fragment } from "react";
+import '../../../styles/sprint_styles.css'
+
 export default function Savanna() {
-  return(
-    <div>Спринт</div>
-  )
+  return (
+    <Fragment>
+      <div>Спринт</div>
+      <div>Timer</div>
+      <div className="game_wrapper">
+        <div className="points_container">
+          <div>Points</div>
+        </div>
+        <div className="game_container">
+          <div className="check_marks_container">
+            Галочки
+            <div className="check_marks"></div>
+          </div>
+          <div className="words_container">
+          <div>Word</div>
+          <div>Translation</div>
+          </div>
+             <div className="buttons_container">
+            <button>Верно</button>
+            <button>Неверно</button>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
